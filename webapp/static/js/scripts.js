550becbc5a51a7044eb7d5b3073c3d129e990ef9
Andrew Archbold <andrew.archbold@quiet.ly>
2016-09-21 19:38:18 -0700

    #time 2h 0m adding comets, resizing sections, and links in footer that scroll to sections.

--- webapp/static/js/scripts.js
+++ webapp/static/js/scripts.js
@@ -1,3 +1,7 @@
+/* eslint-disable */
+
+// I like to move it move it. I like to move it move it.
+// This handles the little parallaxy stuff
 $.fn.moveIt = function(){
   var $window = $(window);
   var instances = [];
@@ -22,7 +26,50 @@ moveItItem.prototype.update = function(scrollTop){
   this.el.css('transform', 'translateY(' + -pos + 'px)');
 };
 
+// udpate section heights when page loads
+// this will update when a user changes their window size
+$.fn.setSectionHeight = function(){
+  var context = $(this),
+    hero = $('.main-hero', context),
+    sections = $('.section-dusk', context),
+    windowHeight = $(window).height(),
+    footerHeight = $('.main-footer', context).height();
+
+  function setHeroHeight() {
+    hero.height(windowHeight - footerHeight);
+    sections.each(function(i,e) {
+      $(e).height(windowHeight - footerHeight)
+    });
+  }
+
+  setHeroHeight();
+
+  $(window).on('resize', _.debounce(function(){
+      // check if window.width() matches function initialized width
+      setHeroHeight('main-hero');
+  }, 500));
+}
+
+// scroll to sections
+$.fn.scrolltoSection = function(){
+  var context = $(this),
+    body = $('html, body'),
+    links = $('a[data-goto]', context),
+    offset = 50;
+
+  links.click(function(e) {
+    e.preventDefault();
+    var target = $("section[data-target='" + $(this).data('goto') +"']");
+    setTimeout(function(){
+      body.stop(true, true).animate({ scrollTop: target.offset().top - offset }, 600);
+    }, 50);
+  })
+}
+
+
 $(function(){
   $('[data-scroll-speed]').moveIt();
+  $('#home-page').setSectionHeight();
+  $('.main-footer').scrolltoSection();
 });
 
