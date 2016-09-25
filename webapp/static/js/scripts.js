cacbdd093c1a1bebc8a2fa14aef90f20d8238506
Andrew Archbold <andrew.archbold@quiet.ly>
2016-09-25 14:13:20 -0700

    #time 1h 0m updating footer for desktop devices and removing parallax effect from mobile devices. Was too laggy

--- webapp/static/js/scripts.js
+++ webapp/static/js/scripts.js
@@ -69,56 +69,64 @@ $.fn.scrolltoSection = function(){
 
 function scrollyStuff() {
   var lastScrollTop = 0,
+    $html = $('html'),
+    $window = $(window),
     feedbackSection = $('.home-feedback'),
     footer = $('.main-footer'),
     btnSlack = $('.section-feedback__cta-slack'),
     btnTwitter = $('.section-feedback__cta-twitter');
-  // $(window).scroll(function(event){
-  //   var st = $(this).scrollTop();
-  //   if (st > lastScrollTop){
-  //   // downscroll code
-  //     footer.addClass('-show-footer');
-  //   } else {
-  //   // upscroll code
-  //   }
-  //   if (st < 50) {
-  //     footer.removeClass('-show-footer');
-  //   }
-  //   if (st >= feedbackSection.offset().top - 400) {
-  //     btnSlack.addClass('-show');
-  //     btnTwitter.addClass('-show');
-  //   } else {
-  //     btnSlack.removeClass('-show');
-  //     btnTwitter.removeClass('-show');    }
-  //   lastScrollTop = st;
-  // });
+
 
   // set OG footer position
-  footer.css({'opacity': 1,'top':($(window).height() + $(this).scrollTop()) - 120});
+  if ($html.hasClass('no-touch')) {
+    $window.scroll(function(event){
+      var st = $(this).scrollTop();
+      if (st > lastScrollTop){
+      // downscroll code
+        footer.addClass('-show-footer');
+      } else {
+      // upscroll code
+      }
+      if (st < 50) {
+        footer.removeClass('-show-footer');
+      }
+      if (st >= feedbackSection.offset().top - 400) {
+        btnSlack.addClass('-show');
+        btnTwitter.addClass('-show');
+      } else {
+        btnSlack.removeClass('-show');
+        btnTwitter.removeClass('-show');    }
+      lastScrollTop = st;
+    });
+  } else {
+    footer.css({'position':'absolute','opacity': 1,'bottom': 'auto','top':($(window).height() + $(this).scrollTop()) - 120});
 
-  $(window).on('scrollstop', function(event) {
-    console.log('show footer');
-    console.log($(this).scrollTop());
-    console.log('window height + scroll top', $(window).height() + $(this).scrollTop());
-    // footer.removeClass('-hide-footer');
-    console.log('body height', $('body').height())
-    footer.css({'top':($(window).height() + $(this).scrollTop()) - 130});
-    footer.show();
-  });
-  $(window).on('scrollstart', function(event) {
-    console.log('hide footer');
-    // footer.addClass('-hide-footer');
-    // footer.css({'opacity': 0})
-    if ($(window).height() + $(this).scrollTop() < $('body').height()) {
-      footer.hide();
-    }
-  });
+    $(window).on('scrollstop', function(event) {
+      console.log('show footer');
+      console.log($(this).scrollTop());
+      console.log('window height + scroll top', $(window).height() + $(this).scrollTop());
+      // footer.removeClass('-hide-footer');
+      console.log('body height', $('body').height())
+      footer.css({'top':($(window).height() + $(this).scrollTop()) - 130});
+      footer.show();
+    });
+    $(window).on('scrollstart', function(event) {
+      console.log('hide footer');
+      // footer.addClass('-hide-footer');
+      // footer.css({'opacity': 0})
+      if ($(window).height() + $(this).scrollTop() < $('body').height()) {
+        footer.hide();
+      }
+    });
+  }
 
 }
 
 
 $(function(){
-  $('[data-scroll-speed]').moveIt();
+  if ($('html').hasClass('no-touch')) {
+    $('[data-scroll-speed]').moveIt();
+  };
   $('#home-page').setSectionHeight();
   $('.main-footer').scrolltoSection();
   // show footer when scrolling down
